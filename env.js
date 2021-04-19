7debe5231381fa62161093fc2b5bdd1ec467b76b
vinny023 <35015236+vinny023@users.noreply.github.com>
2021-04-19 16:50:59 -0400

    new update

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
-// exports.NETLIFY = "https://elastic-beaver-a020d9.netlify.app/.netlify/functions/"
-exports.NETLIFY = 'http://localhost:8888/.netlify/functions/'
+exports.NETLIFY = "https://elastic-beaver-a020d9.netlify.app/.netlify/functions/"
+// exports.NETLIFY = 'http://localhost:8888/.netlify/functions/'
 
 
 exports.headers = {
